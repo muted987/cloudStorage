f3d7cfcb2c3a5549f4ca2948c2c608fa2ebc6741
sunSET <akashi322wp@yandex.ru>
2025-11-04 14:44:31 +0300

    fix: new files and some fixes

--- src/main/resources/db/migration/V1_1_1__BasicUserManagerSchema.sql
+++ src/main/resources/db/migration/V1_1_1__BasicUserManagerSchema.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS user_management;
 
 CREATE TABLE IF NOT EXISTS user_management.t_user(
     id SERIAL PRIMARY KEY,
-    username VARCHAR NOT NULL CHECK(length(trim(username)) > 3) UNIQUE,
-    password VARCHAR NOT NULL CHECK(length(trim(username)) > 8),
+    username VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
     role VARCHAR NOT NULL
 );
