c64c68e5d3140f2786cd18b15cc82efcdceecd58
sunSET <akashi322wp@yandex.ru>
2025-11-04 14:52:12 +0300

    feat: ResourceType enum for ResourceResponse

--- src/main/java/com/muted987/cloudstorage/dto/response/resourceResponse/ResourceType.java
+++ src/main/java/com/muted987/cloudstorage/dto/response/resourceResponse/ResourceType.java
@@ -1,8 +1,19 @@
 package com.muted987.cloudStorage.dto.response.resourceResponse;
 
+
 public enum ResourceType {
     DIRECTORY("DIRECTORY"), FILE("FILE");
 
+    private final String resourceTypeCation;
+
     ResourceType(String resourceTypeCaption) {
+        this.resourceTypeCation = resourceTypeCaption;
     }
+
+    String getResourceTypeCaption(){
+        return resourceTypeCation;
+    }
+
+
+
 }
