14b2efa7057dfcb1253cae67a38cc1844430fcd7
sunSET <akashi322wp@yandex.ru>
2025-11-04 14:49:15 +0300

    style:

--- src/main/java/com/muted987/cloudstorage/exception/UserAlreadyExistException.java
+++ src/main/java/com/muted987/cloudstorage/exception/UserAlreadyExistException.java
@@ -1,5 +1,6 @@
 package com.muted987.cloudStorage.exception;
 
+
 public class UserAlreadyExistException extends RuntimeException {
     public UserAlreadyExistException(String message) {
         super(message);
