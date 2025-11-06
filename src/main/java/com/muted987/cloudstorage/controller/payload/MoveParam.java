8e3eb3453e1389797e3eb5fbdedc538c51eaf2f3
sunSET <akashi322wp@yandex.ru>
2025-11-06 23:00:34 +0300

    docs: added description and example to payload parameters

--- src/main/java/com/muted987/cloudstorage/controller/payload/MoveParam.java
+++ src/main/java/com/muted987/cloudstorage/controller/payload/MoveParam.java
@@ -1,14 +1,17 @@
 package com.muted987.cloudStorage.controller.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record MoveParam(
         @NotNull(message="Отсутствует путь")
         @Pattern(regexp="^(?!.*/\\.(/|$))(?!(?:.*/)?[^/]*\\.[^/]*/)(?:[\\w\\s()-]+/)*[\\w\\s().-]*(?:\\.[\\w]+)?$", message = "Путь невалидный")
+        @Schema(description = "Path from", example = "folder1/file1.txt")
         String from,
         @NotNull(message="Отсутствует путь")
         @Pattern(regexp="^(?!.*/\\.(/|$))(?!(?:.*/)?[^/]*\\.[^/]*/)(?:[\\w\\s()-]+/)*[\\w\\s().-]*(?:\\.[\\w]+)?$", message = "Путь невалидный")
+        @Schema(description = "Path to", example = "folder2/file1.txt")
         String to
 ) {
 }
--- src/main/java/com/muted987/cloudstorage/controller/payload/QueryParam.java
+++ src/main/java/com/muted987/cloudstorage/controller/payload/QueryParam.java
@@ -1,11 +1,13 @@
 package com.muted987.cloudStorage.controller.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record QueryParam(
         @NotNull(message="Отсутствует запрос")
         @Pattern(regexp="^/|(/[\\\\w-]+)+$", message = "Запрос")
+        @Schema(description = "Query", example = "file1.txt")
         String query
 ) {
 }
