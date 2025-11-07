5270544f8ddf780161169ea4cb8e6bd3cc9a3c4d
sunSET <akashi322wp@yandex.ru>
2025-11-07 15:58:56 +0300

    fix: add russian symbols as valid in path

--- src/main/java/com/muted987/cloudstorage/controller/payload/MoveParam.java
+++ src/main/java/com/muted987/cloudstorage/controller/payload/MoveParam.java
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.Pattern;
 
 public record MoveParam(
         @NotNull(message="Отсутствует путь")
-        @Pattern(regexp="^(?!.*/\\.(/|$))(?!(?:.*/)?[^/]*\\.[^/]*/)(?:[\\w\\s()-]+/)*[\\w\\s().-]*(?:\\.[\\w]+)?$", message = "Путь невалидный")
+        @Pattern(regexp="^(?!.*/\\.(/|$))(?!(?:.*/)?[^/]*\\.[^/]*/)(?:[\\w\\s()\\-а-яёА-ЯЁ]+/)*[\\w\\s()\\-.а-яёА-ЯЁ]*(?:\\.[\\wа-яё]+)?$", message = "Путь невалидный")
         @Schema(description = "Path from", example = "folder1/file1.txt")
         String from,
         @NotNull(message="Отсутствует путь")
-        @Pattern(regexp="^(?!.*/\\.(/|$))(?!(?:.*/)?[^/]*\\.[^/]*/)(?:[\\w\\s()-]+/)*[\\w\\s().-]*(?:\\.[\\w]+)?$", message = "Путь невалидный")
+        @Pattern(regexp="^(?!.*/\\.(/|$))(?!(?:.*/)?[^/]*\\.[^/]*/)(?:[\\w\\s()\\-а-яёА-ЯЁ]+/)*[\\w\\s()\\-.а-яёА-ЯЁ]*(?:\\.[\\wа-яё]+)?$", message = "Путь невалидный")
         @Schema(description = "Path to", example = "folder2/file1.txt")
         String to
 ) {
