c71009c59c61db7ef92aaf06f347450af46dc214
sunSET <akashi322wp@yandex.ru>
2025-11-13 23:37:56 +0300

    refactor: changes in several methods
    Changes:
    -Removed getResourceResponses and use new ItemInfoMapper with same logic
    -fixing isParentFolderExist logic

--- src/main/java/com/muted987/cloudstorage/service/minioService/DirectoryService.java
+++ src/main/java/com/muted987/cloudstorage/service/minioService/DirectoryService.java
@@ -5,15 +5,11 @@ import com.muted987.cloudStorage.dto.response.resourceResponse.ResourceResponse;
 import com.muted987.cloudStorage.exception.ParentFolderNotExist;
 import com.muted987.cloudStorage.exception.ResourceAlreadyExistsException;
 import com.muted987.cloudStorage.exception.ResourceNotFoundException;
-import com.muted987.cloudStorage.mapper.ResourceResponseMapper;
-import com.muted987.cloudStorage.repository.MinioS3Repository;
+import com.muted987.cloudStorage.mapper.resourceResponseMapper.ResourceResponseMapper;
+import com.muted987.cloudStorage.repository.s3Repository.S3Repository;
 import com.muted987.cloudStorage.utils.PathUtil;
-import io.minio.ObjectWriteResponse;
-import io.minio.Result;
-import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -24,7 +20,7 @@ import java.util.List;
 @Slf4j
 public class DirectoryService {
 
-    private final MinioS3Repository minioS3Repository;
+    private final S3Repository minioRepository;
     private final ResourceResponseMapper resourceResponseMapper;
 
     public List<ResourceResponse> getDirectory(String requestPath, int id) {
@@ -35,22 +31,22 @@ public class DirectoryService {
             throw new ResourceNotFoundException("Папка не существует");
         }
 
-        Iterable<Result<Item>> result = minioS3Repository.getListObjects(formattedPath);
+        List<ItemInfo> result = this.minioRepository.getListObjects(formattedPath);
 
-        return getResourceResponses(result, formattedPath);
+        return this.resourceResponseMapper.toResourceResponseList(result);
     }
 
     public List<ResourceResponse> getDirectoryRecursive(String requestPath, int id) {
-        String refactoredPath = PathUtil.formatPath(id, requestPath);
+        String formattedPath = PathUtil.formatPath(id, requestPath);
 
         if (!isFolderExist(requestPath, id)) {
             log.warn("Исключение выброшено в методе getDirectoryRecursive");
             throw new ResourceNotFoundException("Папка не существует");
         }
 
-        Iterable<Result<Item>> result = minioS3Repository.getListObjectsRecursive(refactoredPath);
+        List<ItemInfo> result = this.minioRepository.getListObjectsRecursive(formattedPath);
 
-        return getResourceResponses(result, refactoredPath);
+        return this.resourceResponseMapper.toResourceResponseList(result);
     }
 
     public void createRootFolder(int id){
@@ -62,34 +58,27 @@ public class DirectoryService {
         }
 
         try {
-            this.minioS3Repository.createDirectory(rootFolderPath);
+            this.minioRepository.createDirectory(rootFolderPath);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public DirectoryResponse createFolder(String path, int id) {
+    public DirectoryResponse createFolder(String requestPath, int id) {
 
-        if (!isParentFolderExist(path, id)) {
+        if (!isParentFolderExist(requestPath, id)) {
             log.warn("Исключение выброшено в методе createFolder");
             throw new ParentFolderNotExist("Родительская папка не существует");
         }
 
-        if (isFolderExist(path, id)) {
+        if (isFolderExist(requestPath, id)) {
             log.warn("Исключение выброшено в методе createFolder");
             throw new ResourceAlreadyExistsException("Папка уже существует");
         }
 
-        String formattedPath = PathUtil.formatPath(id, path);
-        ObjectWriteResponse result;
-
-        try {
-            result = this.minioS3Repository.createDirectory(formattedPath);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return this.resourceResponseMapper.toDirectoryResponse(result, formattedPath);
+        String formattedPath = PathUtil.formatPath(id, requestPath);
+        ItemInfo itemInfo = this.minioRepository.createDirectory(formattedPath);
+        return (DirectoryResponse) this.resourceResponseMapper.toResourceResponse(itemInfo);
     }
 
     public List<ResourceResponse> createParentFolders(String path, int id) {
@@ -110,41 +99,16 @@ public class DirectoryService {
         return resourceResponses;
     }
 
-    private @NotNull List<ResourceResponse> getResourceResponses(Iterable<Result<Item>> result, String formattedPath) {
-        List<ResourceResponse> resourceResponses = new ArrayList<>();
-        for (Result<Item> item : result) {
-            try {
-                Item object = item.get();
-                if (PathUtil.reformatPath(object.objectName()).isEmpty() || object.objectName().equals(formattedPath)) {
-                    continue;
-                }
-                if (object.objectName().endsWith("/")) {
-                    resourceResponses.add(this.resourceResponseMapper.toDirectoryResponse(object, formattedPath));
-                } else {
-                    resourceResponses.add(this.resourceResponseMapper.toFileResponse(object, formattedPath));
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return resourceResponses;
-    }
-
     private boolean isFolderExist(String path, int id) {
         if (isRootFolder(path)) {
             return true;
         }
-        return this.minioS3Repository.isObjectExist(PathUtil.formatPath(id, path));
+        return this.minioRepository.isObjectExist(PathUtil.formatPath(id, path));
     }
 
     private boolean isParentFolderExist(String path, int id) {
         String parentPath = PathUtil.getParentPath(path);
-        String parentPathOfParentPath = PathUtil.getParentPath(parentPath);
-        if (parentPathOfParentPath.isEmpty()) {
-            return true;
-        }
-        return getDirectory(parentPathOfParentPath, id).stream()
-                .anyMatch(resourceResponse -> resourceResponse.getPath().concat(resourceResponse.getName()).equals(parentPath));
+        return isFolderExist(parentPath, id);
     }
 
     private boolean isRootFolder(String path) {
